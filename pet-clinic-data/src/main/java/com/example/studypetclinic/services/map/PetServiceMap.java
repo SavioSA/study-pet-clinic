4a7a4caf7a81f53d542cac77bd513826fc4a0442
SavioSA <saviosilvadearaujo2019@gmail.com>
2024-07-27 20:00:17 -0300

    Implement Map Based Services. closes #6

---
+++ pet-clinic-data/src/main/java/com/example/studypetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,38 @@
+package com.example.studypetclinic.services.map;
+
+import com.example.studypetclinic.model.Pet;
+import com.example.studypetclinic.services.CrudService;
+import com.example.studypetclinic.services.map.AbstractMapService;
+
+
+import java.util.Set;
+
+/**
+ * Created by jt on 7/21/18.
+ */
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
